f5d3905333c9bd0977ee7dae802fe1a7e374c7ce
mikonore <51318236+mikonore@users.noreply.github.com>
2020-09-28 14:25:50 +0200

    Create UDP_send.py

---
+++ software/UDP_send.py
@@ -0,0 +1,18 @@
+import socket
+import numpy as np
+UDP_IP = '169.254.52.219'
+UDP_PORT = 8888
+#MESSAGE = "Hello, World!"
+
+
+print("UDP target IP: %s" % UDP_IP)
+print("UDP target port: %s" % UDP_PORT)
+
+
+sock = socket.socket(socket.AF_INET, # Internet
+                     socket.SOCK_DGRAM) # UDP
+
+for i in range(10):
+    MESSAGE = str(np.random.randint(-1000,1000))
+    sock.sendto(MESSAGE.encode(), (UDP_IP, UDP_PORT))
+    print("message: %s" % MESSAGE)
